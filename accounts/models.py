b2bbea558a3cfee22f656028fd70d639bef5f36e
Lasvegasman <ftwalton31@yahoo.com>
2025-03-01 17:16:48 +0700

    Final Report

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class CustomUser(AbstractUser):
+    # Add any additional fields here
+    pass
 
 # Create your models here.
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    # Add any additional fields here
+    pass
+
 # Create your tests here.
